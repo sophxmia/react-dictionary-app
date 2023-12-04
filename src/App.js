7a47546adc4cb3168840bbb6270ca7299538f3ec
sophxmia <maliarenko.sofiia@gmail.com>
2023-12-04 17:26:03 +0200

    Added basic structure to the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import logo from "./logo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container-fluid">
+        <header className="App-header">
+          <a
+            href="https://brilliant-meerkat-d49135.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={logo} className="logo" alt="Header Logo" />
+          </a>
+          <h1 className="text-center fw-bold fs-1 p-3">
+            Dictionary Application
+          </h1>
+        </header>
+        <Dictionary />
+        <footer className="mt-5">
+          Open-sourse project,{" "}
+          <a
+            href="https://github.com/sophxmia/react-weather-forecast-app"
+            target="_blank"
+            rel="noreferrer"
+            title="GitHub link"
+          >
+            hosted on GitHub
+          </a>
+          , written by{" "}
+          <a
+            href="https://brilliant-meerkat-d49135.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            title="Netlify link to the project"
+          >
+            Sofiia Maliarenko
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Dictionary.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  return (
+    <div className="Dictionary">
+      <section>
+        <form>
+          <label>Welcome to the Dictionary? What is your search today?</label>
+          <div className="row my-3">
+            <div className="col-sm-7">
+              <input
+                type="search"
+                placeholder="Enter your search..."
+                className="form-control mb-3"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-sm-2 mb-1">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <small className="hint">Examples:coding, program, developer</small>
+      </section>
+    </div>
+  );
+}
