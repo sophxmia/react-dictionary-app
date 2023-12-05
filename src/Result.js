6ae84849dd07cd97adc05feabc0a1c77295267c6
sophxmia <maliarenko.sofiia@gmail.com>
2023-12-05 13:41:49 +0200

    Added new component Phonetic

--- src/Result.js
+++ src/Result.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import "./Result.css";
 
 export default function Result(props) {
   if (props.result) {
-    return <div className="Result"></div>;
+    return (
+      <div className="Result">
+        <section>
+          <h1>{props.result[0].word}</h1>
+          <Phonetic phonetic={props.result[0].phonetics} />
+        </section>
+      </div>
+    );
   } else {
     return null;
   }
