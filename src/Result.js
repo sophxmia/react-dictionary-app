a47dc7ac1edd7655dc4f4194acfd83673ab58be8
sophxmia <maliarenko.sofiia@gmail.com>
2023-12-08 16:14:22 +0200

    Fixed the bug with audio

--- src/Result.js
+++ src/Result.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
 import "./Result.css";
 
 export default function Result(props) {
+  useEffect(() => {
+    if (props.result) {
+      const audioElement = document.getElementById("audioElement");
+      audioElement.src = `https://api.dictionaryapi.dev/media/pronunciations/en/${props.result[0].word}-uk.mp3`;
+    }
+  }, [props.result]);
+
   if (props.result) {
     return (
       <div className="Result">
         <section>
           <h2>{props.result[0].word}</h2>
           <Phonetic phonetic={props.result[0].phonetics} />
-          <audio controls className="mt-2">
-            {props.result[0].phonetics.map((phonetic, index) => (
-              <source
-                key={index}
-                src={phonetic.audio}
-                type="audio/mpeg"
-              ></source>
-            ))}
+          <audio controls className="mt-2" id="audioElement">
+            <source
+              src={`https://api.dictionaryapi.dev/media/pronunciations/en/${props.result[0].word}-uk.mp3`}
+              type="audio/mpeg"
+            ></source>
             Your browser does not support the audio element.
           </audio>
         </section>
