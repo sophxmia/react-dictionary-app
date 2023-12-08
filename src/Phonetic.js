fd8c403b7384fd63adce39f32134a69398a75576
sophxmia <maliarenko.sofiia@gmail.com>
2023-12-08 15:47:44 +0200

    Catch a bug in audio

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -6,10 +6,6 @@ export default function Phonetic(props) {
     return (
       <div className="Phonetic">
         <h4>{props.phonetic[1].text}</h4>
-        <audio controls className="mt-2">
-          <source src={props.phonetic[0].audio} type="audio/mpeg"></source>
-          Your browser does not support the audio element.
-        </audio>
       </div>
     );
   } else {
--- src/Result.js
+++ src/Result.js
@@ -10,6 +10,16 @@ export default function Result(props) {
         <section>
           <h2>{props.result[0].word}</h2>
           <Phonetic phonetic={props.result[0].phonetics} />
+          <audio controls className="mt-2">
+            {props.result[0].phonetics.map((phonetic, index) => (
+              <source
+                key={index}
+                src={phonetic.audio}
+                type="audio/mpeg"
+              ></source>
+            ))}
+            Your browser does not support the audio element.
+          </audio>
         </section>
         {props.result[0].meanings.map(function (meaning, index) {
           return (
