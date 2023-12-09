223e7828ed4417e191d0de25231eee5d71cd59ce
sophxmia <maliarenko.sofiia@gmail.com>
2023-12-09 12:34:39 +0200

    Update Phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  if (props.phonetic) {
+  if (props.phonetic && props.phonetic[1] && props.phonetic[1].text) {
     return (
       <div className="Phonetic">
         <h4>{props.phonetic[1].text}</h4>
