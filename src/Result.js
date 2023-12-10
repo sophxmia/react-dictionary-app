9ad05452559646d82b2007818a00a623763d806e
sophxmia <maliarenko.sofiia@gmail.com>
2023-12-10 23:21:56 +0200

    Fix the bug with audio

--- src/Result.js
+++ src/Result.js
@@ -1,22 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
 import "./Result.css";
 
 export default function Result(props) {
+  const [audioSrc, setAudioSrc] = useState("");
+
   useEffect(() => {
     if (props.result) {
-      const audioElement = document.getElementById("audioElement");
-      const word = props.result[0].word;
-
-      // Check if the UK pronunciation is available, otherwise use the US pronunciation
-      const ukPronunciationUrl = `https://api.dictionaryapi.dev/media/pronunciations/en/${word}-uk.mp3`;
-      const usPronunciationUrl = `https://api.dictionaryapi.dev/media/pronunciations/en/${word}-us.mp3`;
-
-      audioElement.src =
-        props.result[0].phonetics[0]?.audio || usPronunciationUrl;
+      setAudioSrc(getAudioSource(props.result[0]));
     }
   }, [props.result]);
+
+  const getAudioSource = (result) => {
+    for (const phonetic of result.phonetics) {
+      if (phonetic.audio) {
+        return phonetic.audio;
+      }
+    }
+
+    return result.sourceUrls[0] || "";
+  };
 
   if (props.result) {
     return (
@@ -24,11 +28,7 @@ export default function Result(props) {
         <section>
           <h2>{props.result[0].word}</h2>
           <Phonetic phonetic={props.result[0].phonetics} />
-          <audio controls className="mt-2" id="audioElement">
-            <source
-              src={`https://api.dictionaryapi.dev/media/pronunciations/en/${props.result[0].word}-uk.mp3`}
-              type="audio/mpeg"
-            ></source>
+          <audio controls className="mt-2" id="audioElement" src={audioSrc}>
             Your browser does not support the audio element.
           </audio>
         </section>
